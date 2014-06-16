0a0558ad97e4e49d163a77e0c700552edf4122f0
Mike Chlipala <mike@chlipala.com>
2014-06-16 15:42:39 -0700

    Add bulk create endpoint for documents

--- test/documentsTest.js
+++ test/documentsTest.js
@@ -29,6 +29,23 @@ describe('documents', function() {
     })
   })
 
+  it('creates multiple documents', function(done) {
+    var documents = [
+      {external_id: '3', fields: [ { name: 'title', value: 'A Great Book', type: 'string' } ]},
+      {external_id: '4', fields: [ { name: 'title', value: 'Another Great Book', type: 'string' } ]},
+    ]
+
+    client.documents.bulkCreate({
+      engine: engine,
+      documentType: documentType,
+      documents: documents
+    }, function(err, res) {
+      assert(res)
+      assert.equal(2, res.length)
+      done()
+    })
+  })
+
   it('gets a document', function(done) {
     client.documents.get({
       engine: engine,
